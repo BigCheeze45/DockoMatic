cc89fa4d196b671ddb7a921b3e8bd30f947d9da6
wbullock <wbullock@851e0734-0dea-0310-9eb6-8f37a80d29b1>
2011-08-25 20:55:15 +0000

    Added ability to align different subunits of template.  Removed Kill Job buttons from view of wizard windows 3 and 5, since they will not be implemented at this time.  Fixed job status; wasn't showing as started.  Bulk submit, so Swarm jobs per node works now.

--- lib/scripts/modeller/align2d.py
+++ lib/scripts/modeller/align2d.py
@@ -6,6 +6,7 @@ import os
 seq = sys.argv[1]
 tmpl = sys.argv[2]
 out = sys.argv[3]
+sub = sys.argv[4]
 
 #print seq+'.ali'
 #print tmpl+'.pdb'
@@ -17,12 +18,12 @@ from modeller import *
 
 env = environ()
 aln = alignment(env)
-mdl = model(env, file=tmpl, model_segment=('FIRST:@','END:'))
+mdl = model(env, file=tmpl, model_segment=('FIRST:'+sub,'END:'+sub))
 aln.append_model(mdl, align_codes=tmpl, atom_files=tmpl+'.pdb')
 aln.append(file=seq+'.ali', align_codes=seq)
 aln.align2d()
-aln.write(file=out+'/'+seq+'-'+tmpl+'.ali', alignment_format='PIR')
-aln.write(file=out+'/'+seq+'-'+tmpl+'.pap', alignment_format='PAP')
+aln.write(file=out+'/'+seq+'.pir', alignment_format='PIR')
+aln.write(file=out+'/'+seq+'.pap', alignment_format='PAP')
 
 os.system("ls *.pap > alLog");
 
