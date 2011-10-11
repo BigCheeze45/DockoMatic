4ad3567d42d22c6251b00b00ce949abc625ffb51
wbullock <wbullock@851e0734-0dea-0310-9eb6-8f37a80d29b1>
2011-10-11 18:52:03 +0000

    Changed naming of .pir files to _pir.ali ( modeling stuff).  Changed font of text areas used to modify alignment for modeling.  Added functionality to run each individual modeling job in parallel.

--- trunk/src/DockoMatic/lib/scripts/modeller/model-single.py
+++ trunk/src/DockoMatic/lib/scripts/modeller/model-single.py
@@ -9,12 +9,12 @@ out = sys.argv[4]
 from modeller import *
 from modeller.automodel import *
 
-env = environ()
-a = automodel(env, alnfile=out+'/'+seq+'.pir',
+env = environ(rand_seed = -2 - int(num))
+a = automodel(env, alnfile=out+'/'+seq+'_pir.ali',
               knowns=tmpl, sequence=seq,
               assess_methods=(assess.DOPE, assess.GA341))
-#a.starting_model = int(num)
-a.starting_model = 1
-a.ending_model = int(num)
+a.starting_model = a.ending_model = int(num)
+#a.starting_model = 1
+#a.ending_model = int(num)
 a.make()
 
