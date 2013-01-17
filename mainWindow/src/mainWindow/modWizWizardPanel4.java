6fd27c3615fb3e2999a2f79fcf6ddde33e7cf008
ncornia <ncornia@851e0734-0dea-0310-9eb6-8f37a80d29b1>
2013-01-17 21:39:31 +0000

    Added GNU LGPL license header to .java files

--- mainWindow/src/mainWindow/modWizWizardPanel4.java
+++ mainWindow/src/mainWindow/modWizWizardPanel4.java
@@ -1,3 +1,35 @@
+# Program: "DockoMatic"
+# Project: "DNA Safeguard"
+# Filename: "modWizWizardPanel4.java"
+#
+# Dr. Tim Andersen
+# Department of Computer Science
+# College of Engineering
+# Boise State University
+#
+# Original Author(s): "Casey Bullock"
+#                     "Nic Cornia"
+#
+# Last Modified
+#   Date: "November 14, 2012"
+#
+#
+#  This file is part of DockoMatic.
+#
+#  DockoMatic is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  DockoMatic is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with DockoMatic.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
